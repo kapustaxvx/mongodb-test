bd8400bcca18cd915ae8972556d11fc6678d1d96
Ilia Moskalenko <i.moskalenkoxvx@gmail.com>
2023-01-09 12:31:22 +0300

    simple task

--- src/main/java/com/example/mongotest/MongotestApplication.java
+++ src/main/java/com/example/mongotest/MongotestApplication.java
@@ -1,7 +1,11 @@
 package com.example.mongotest;
 
+import com.example.mongotest.model.Customer;
+import com.example.mongotest.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MongotestApplication {
@@ -10,4 +14,14 @@ public class MongotestApplication {
         SpringApplication.run(MongotestApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner run(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.deleteAll();
+            customerRepository.save(new Customer("John", "Doe"));
+            customerRepository.save(new Customer("Jane", "Doe"));
+
+            customerRepository.findAll().forEach(System.out::println);
+        };
+    }
 }
